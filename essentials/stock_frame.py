d2a7d9d2322a4a039a5c036e38efea2e03724b78
Jesper Eriksson <je223gs@student.lnu.se>
2020-12-04 16:45:52 +0100

    Adding properties to StockFrame

--- essentials/stock_frame.py
+++ essentials/stock_frame.py
@@ -13,6 +13,29 @@ from pandas.core.groupby import DataFrameGroupBy
 from pandas.core.window import RollingGroupby
 
 
-class stock_frame:
+class StockFrame:
 
-    pass
+    def __init__(self, data: List[dict]) -> None:
+
+        self._data = data
+        self._frame = pd.DataFrame = self.create_frame()
+        self._symbol_groups: DataFrameGroupBy = None
+        self._symbol_rolling_groups: RollingGroupby = None
+        
+    @property
+    def frame(self) -> pd.DataFrame:
+        return self._frame
+    
+    @@property
+    def symbol_groups(self) -> DataFrameGroupBy:
+
+        self._symbol_groups = self._frame.groupby(
+            by='symbol',
+            as_index=False,
+            sort=True
+        )
+
+        return self._symbol_groups
+
+    def _symbol_rolling_groups(self, size: int) -> RollingGroupby:
+        pass
