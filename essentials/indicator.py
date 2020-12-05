7a089ae2772993b543b7a8aa12b24310e4e69fc4
je223gs <je223gs@student.lnu.se>
2020-12-06 00:02:43 +0100

    Adding get and set for indicators

--- essentials/indicator.py
+++ essentials/indicator.py
@@ -2,7 +2,7 @@ import operator
 import numpy as np
 import pandas as pd
 
-from typing import List, Tuple
+from typing import List, Tuple, Any
 from typing import Dict
 from typing import Union
 from typing import Optional
@@ -11,7 +11,32 @@ from essentials.stock_frame import StockFrame
 
 
 class Indicator:
+
     def __init__(self, price_data_frame: StockFrame) -> None:
 
         self._stock_frame: StockFrame = price_data_frame
-        self._price_groups = price_data_frame.symbol_groups
+        self._price_groups = self._stock_frame.symbol_groups
+        self._current_indicators = {}
+        self._indicator_signals = {}
+        self._frame = self._stock_frame.frame
+
+    def set_indicator_signals(self, indicator: str, buy: float, sell: float, condition_buy: Any, condition_sell: Any) -> None:
+
+        # If there is no signal for indicator set empty template
+        if indicator not in self._indicator_signals:
+            self._indicator_signals[indicator] = {}
+
+        # Modify the old one or empty template
+        self._indicator_signals[indicator]['buy'] = buy
+        self._indicator_signals[indicator]['sell'] = sell
+        self._indicator_signals[indicator]['buy_operator'] = condition_buy
+        self._indicator_signals[indicator]['sell_operator'] = condition_sell
+
+    def get_indicator_signals(self, indicator: Optional[str]) -> dict:
+
+        if indicator and indicator in self._indicator_signals:
+            return self._indicator_signals[indicator]
+        else:
+            return self._indicator_signals
+
+
